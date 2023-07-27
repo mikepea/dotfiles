a27bb43b5734754eba39fdd9f1665e36a5239b67
Mike Pountney <Mike.Pountney@gmail.com>
2023-07-27 11:04:58 -0700

    nvim lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,26 @@
+
+
+vim.g.mapleader = ' '
+
+-- See https://github.com/folke/lazy.nvim#-installation
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup('mikepea.plugins')
+
 -- See https://martinlwx.github.io/en/config-neovim-from-scratch/
 --
 
-require('options')
-require('keymaps')
-require('plugins')
-require('colorscheme')
-
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
-})
-
-require('go').setup()
+require('mikepea.options')
+require('mikepea.keymaps')
